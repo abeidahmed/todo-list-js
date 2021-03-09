4b07820117ebd7f622d1e24e8b5977a73b277ff7
abeidahmed <abeidahmed92@gmail.com>
2021-03-09 22:01:53 +0530

    Make page title dynamic

--- src/views/sidebar.js
+++ src/views/sidebar.js
@@ -1,6 +1,7 @@
 import renderTodoItem from './todo';
 
 const projects = JSON.parse(localStorage.getItem('projects'));
+const pageTitle = document.getElementById('project-main-title');
 
 const createButton = (buttonText) => {
   const button = document.createElement('button');
@@ -11,6 +12,7 @@ const createButton = (buttonText) => {
   button.addEventListener('click', () => {
     localStorage.setItem('activeProject', JSON.stringify(buttonText));
     renderTodoItem(buttonText);
+    pageTitle.textContent = buttonText;
   });
 
   return button;
