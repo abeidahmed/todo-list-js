c80b2f623082471af32c7833ed8d6d91191fa441
delicelydia <Delydia84@gmail.com>
2021-03-09 11:16:08 +0200

    Add todo class
    - Added todo array in the project object
    - Added table elements dynamically

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
 import './stylesheets/index.scss';
 
-const projectNavbar = document.getElementById('project-navbar');
+const projectNavbar = document.getElementById('projetttctctct-navbar');
+const tableBody = document.getElementById('table-body');
 
 const projects = [
   {
     title: 'Coding',
+    todos: [
+      {
+        title: 'coding',
+        description: 'javascript coding challenge',
+        dueDate: '30/03/2021',
+        priority: 2,
+      },
+    ],
   },
   {
     title: 'Workout',
+    todos: [
+      {
+        title: 'coding',
+        description: 'javascript coding challenge',
+        dueDate: '30/03/2021',
+        priority: 2,
+      },
+    ],
   },
   {
     title: 'Climbing',
+    todos: [
+      {
+        title: 'coding',
+        description: 'javascript coding challenge',
+        dueDate: '30/03/2021',
+        priority: 2,
+      },
+    ],
   },
 ];
 
@@ -29,3 +54,21 @@ const project = new Project();
 projects.forEach(({ title }) => {
   project.listProject(title);
 });
+
+class Todo {
+  listTodos() {
+    const tableRow = document.createElement('tr');
+    const tableData1 = document.createElement('td');
+    tableData1.classList.add('table-td');
+    const tableData2 = document.createElement('td');
+    tableData2.classList.add('table-td', 'right');
+    const tableData3 = document.createElement('td');
+    tableData3.classList.add('table-td', 'right');
+    tableRow.append(tableData1);
+    tableRow.append(tableData2);
+    tableRow.append(tableData3);
+    tableBody.append(tableRow);
+  }
+}
+
+const todo = new Todo();
