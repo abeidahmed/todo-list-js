39376301901f5bec963d64b597cfdfee087f994d
abeidahmed <abeidahmed92@gmail.com>
2021-03-09 13:50:00 +0530

    List out projects in the sidebar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './stylesheets/index.scss';
+
+const projectNavbar = document.getElementById('project-navbar');
+
+const projects = [
+  {
+    title: 'Coding',
+  },
+  {
+    title: 'Workout',
+  },
+  {
+    title: 'Climbing',
+  },
+];
+
+class Project {
+  // eslint-disable-next-line class-methods-use-this
+  listProject(projectName) {
+    const wrapper = document.createElement('button');
+    wrapper.setAttribute('type', 'button');
+    wrapper.textContent = projectName;
+    projectNavbar.append(wrapper);
+  }
+}
+
+const project = new Project();
+
+projects.forEach(({ title }) => {
+  project.listProject(title);
+});
