329b986734f1c54e2048a4da676da8578d655919
abeidahmed <abeidahmed92@gmail.com>
2021-03-09 15:30:46 +0530

    Add svg icons and render on single todo

--- src/models/todo.js
+++ src/models/todo.js
@@ -12,14 +12,32 @@ export default class Todo {
     this.todos.forEach((todo) => {
       const tableRow = document.createElement('tr');
       const todoTitle = document.createElement('td');
-      todoTitle.textContent = todo.title;
+      const titleContainer = document.createElement('div');
+      titleContainer.classList.add('todo-checker');
+      const todoCheckbox = document.createElement('input');
+      todoCheckbox.setAttribute('type', 'checkbox');
+      titleContainer.append(todoCheckbox);
+      const todoSpan = document.createElement('span');
+      todoSpan.textContent = todo.title;
+      titleContainer.append(todoSpan);
       todoTitle.classList.add('table-td');
+      todoTitle.append(titleContainer);
 
       const todoDueDate = document.createElement('td');
       todoDueDate.classList.add('table-td', 'right');
       todoDueDate.textContent = todo.dueDate;
 
       const todoAction = document.createElement('td');
+      const editButton = document.createElement('button');
+      editButton.setAttribute('type', 'button');
+      editButton.textContent = 'Edit';
+      todoAction.append(editButton);
+
+      const deleteButton = document.createElement('button');
+      deleteButton.setAttribute('type', 'button');
+      deleteButton.textContent = 'Delete';
+      todoAction.append(deleteButton);
+
       todoAction.classList.add('table-td', 'right');
 
       tableRow.append(todoTitle);
