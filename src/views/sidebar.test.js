751d9a2ec2e0430820a49b62b0ad9f5a744a58b0
abeidahmed <abeidahmed92@gmail.com>
2021-03-19 14:51:00 +0530

    Test renderedButtons on the app sidebar

---
+++ src/views/sidebar.test.js
@@ -0,0 +1,38 @@
+import renderProjectButton from './sidebar';
+
+const projects = [
+  {
+    title: 'Coding',
+    todos: [
+      {
+        id: 1,
+        title: 'coding todo 1',
+        description: 'javascript coding challenge',
+        dueDate: '2021-03-04',
+        priority: 'low',
+        isCompleted: false,
+      },
+    ],
+  },
+];
+
+const initializeProject = ({ activeProject = 'Coding', projects }) => {
+  localStorage.setItem('activeProject', JSON.stringify(activeProject));
+  localStorage.setItem('projects', JSON.stringify(projects));
+};
+
+afterEach(() => {
+  localStorage.removeItem('activeProject');
+  localStorage.removeItem('projects');
+});
+
+test('it lists all the projects in the sidebar', () => {
+  document.body.innerHTML =
+    '<nav id="project-navbar" class="sidebar-navigation"></nav>';
+
+  initializeProject({ projects });
+  renderProjectButton();
+  const container = document.getElementById('project-navbar');
+  const buttons = container.querySelectorAll('button');
+  expect(buttons.length).toBe(1);
+});
