45acfb6eec2e8a1af73bd4a8aed54f8b6ede6a5c
delicelydia <Delydia84@gmail.com>
2021-03-19 11:47:44 +0200

    Add projectForm test

---
+++ src/views/projectForm.test.js
@@ -0,0 +1,13 @@
+import renderProjectForm from './projectForm';
+
+test('it should render a project form', () => {
+  document.body.innerHTML = `
+  <div id="sidebar-id" class="sidebar-container">
+  <h2 class="sidebar-heading">Projects</h2>
+  <nav id="project-navbar" class="sidebar-navigation"></nav>
+  </div>
+  `;
+  renderProjectForm();
+  const sidebar = document.getElementById('sidebar-id').children;
+  expect(sidebar[2].nodeName === 'FORM').toBe(true);
+  }); 
